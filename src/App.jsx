db43546516cfbaac99e237bb1a4397f1b1408878
souvik012 <souvikdinda29@gmail.com>
2025-01-28 23:30:00 +0530

    Create Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import './App.css'
+// import './App.css'
+import Home from './Pages/Home'
+import SignIn from './Pages/SignIn'
+import SignOut from './Pages/SignOut'
+import Profile from './Pages/Profile'
+import About from './Pages/About'
 
 function App() {
   
 
-  return (
-    <div class="flex flex-col items-center p-7 rounded-2xl text-red-600 bg-red-300">
-  <div>
-    <img class="size-48 shadow-xl " alt="" src="https://cdn.pixabay.com/photo/2024/09/19/18/08/rose-9059411_640.jpg" />
-  </div>
-  <div class="flex">
-    <br /><p>Class Warfare</p><br />
-    <p>The Anti-Patterns</p>
-    <span class="flex">
-      <span>No. 4</span>
-      <span>·</span>
-      <span>2025</span>
-    </span>
-  </div>
-</div>
+  return (<BrowserRouter>
+    <Routes>
+         <Route path='/' element={<Home />}/>
+         <Route path='/SignIn' element={<SignIn />}/>
+         <Route path='/SignOut' element={<SignOut />}/>
+         <Route path='/Profile' element={<Profile />}/>
+         <Route path='/About' element={<About />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Pages/SignOut.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ function SignOut() {
+  return (
+    <div>SignOut</div>
+  )
+}
+export default SignOut
