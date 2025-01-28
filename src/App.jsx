7efda7b170b8ee3e8228258d1145bd4303748830
souvik012 <souvikdinda29@gmail.com>
2025-01-28 21:08:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div class="flex flex-col items-center p-7 rounded-2xl text-red-600 bg-red-300">
+  <div>
+    <img class="size-48 shadow-xl " alt="" src="https://cdn.pixabay.com/photo/2024/09/19/18/08/rose-9059411_640.jpg" />
+  </div>
+  <div class="flex">
+    <br /><p>Class Warfare</p><br />
+    <p>The Anti-Patterns</p>
+    <span class="flex">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+  </div>
+</div>
+  )
+}
+
+export default App
