02e8c6784c2d1714ee01d96ca2774e1336314ed0
souvik012 <souvikdinda29@gmail.com>
2025-06-08 12:36:59 +0530

    Complete header search form Functionality

--- Client/RealEstate/src/Components/Header.jsx
+++ Client/RealEstate/src/Components/Header.jsx
@@ -1,14 +1,35 @@
 import {FaSearch} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {Link , useNavigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
 
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function Header() {
   const {currentuser} = useSelector(state =>state.user)
+  const [searchTerm , setSearchTerm] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+
   console.log("Current User:", currentuser);
   console.log("Avatar URL from Redux:", currentuser?.avatar);
+  
 
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
 
   return (
     <header className='bg-sky-200 shadow-md'>
@@ -25,16 +46,18 @@ function Header() {
 
      {/* this div is changed */}
       <div className="flex-1 flex justify-center">
-       <form className='bg-slate-100 p-2 rounded-lg flex items-center' >
+       <form onSubmit={handleSubmit} className='bg-slate-100 p-2 rounded-lg flex items-center' >
          
         <input 
         type="text" 
         placeholder='Search...'
         className='bg-transparent focus:outline-none w-24 sm:w-64'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
         />
-
-        <FaSearch className='text-sky-900'/>
-          
+        <button>
+           <FaSearch className='text-sky-900'/>
+        </button>  
        </form>
        </div>
        {/* list of home to sign in */}
